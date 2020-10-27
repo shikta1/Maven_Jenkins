87b676b7d0bd4052693b069b296d16809f8ef0c1
SD <SD@gmail.com>
2020-10-27 11:07:44 +0530

    new print messsage

--- MyJenkins_Maven/src/main/java/com/data/App.java
+++ MyJenkins_Maven/src/main/java/com/data/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Eclipse Jenkins");
+		System.out.println("Welcome to Eclipse Jenkins 123");
 	}
 
 }
