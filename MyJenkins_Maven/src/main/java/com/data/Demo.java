3fa114b6bc5c76cbcd70c608bced5887db6dad77
SD <SD@gmail.com>
2020-10-28 08:30:17 +0530

    junit testcase

---
+++ MyJenkins_Maven/src/main/java/com/data/Demo.java
@@ -0,0 +1,10 @@
+package com.data;
+
+public class Demo {
+
+	public int add(int x,int y)
+	{
+	return x*y;
+	}
+
+}
---
+++ MyJenkins_Maven/src/test/java/TestDemo.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.data.Demo;
+
+public class TestDemo {
+
+@Test
+public void testAdd() {
+Demo d = new Demo();
+int ans = d.add(20, 20);
+assertEquals(400, ans);
+}
+
+}
+
